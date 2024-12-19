d68993cf445a8b85dc36f1bb16f3ba394ef8935a
anathanP <abbasmasoudardakani@gmail.com>
2024-12-19 08:48:05 +0330

    Enabling admin and playing with orm

--- playground/views.py
+++ playground/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.db.models import F, Count, Sum
+from Store.models import Collection, Customer, Product
 
 # Create your views here.
 def say_hello(request) -> HttpResponse:
-    return render(request, 'hello.html', {'name': 'Abbas'})
+    orders = Product.objects.annotate(
+        total_sale=Sum(
+            F('orderitem__quantity')*F("orderitem__unit_price")
+        )
+    ).order_by("-total_sale")[:5]
+
+    return render(request, 'hello.html', {'name': 'Abbas', "orders": list(orders)})
 
